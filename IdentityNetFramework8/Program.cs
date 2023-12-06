94c49eafcf1006ece8ad1ae5e4dcb9dde6bae118
Atef Ataya <atef.ataya>
2023-12-06 19:14:24 +0400

    Configure Program.cs

--- IdentityNetFramework8/Program.cs
+++ IdentityNetFramework8/Program.cs
@@ -1,3 +1,8 @@
+using IdentityNetFramework8;
+using IdentityNetFramework8.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +12,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Setup Identity - Configure the identity to use cookie-based authentication and enable authorization
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+builder.Services.AddAuthorization();
+
+// Configure the EF Core Database. Use the in-memory database 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+
+// Configure identity to use the EF Core database & expose the identity endpoints
+builder.Services.AddIdentityCore<MyUser>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
 var app = builder.Build();
+
+// Map the routes for the identity endpoints
+app.MapIdentityApi<MyUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
